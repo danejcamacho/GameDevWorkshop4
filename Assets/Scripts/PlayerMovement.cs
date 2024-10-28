304ce923f7d154b8243a4b7416dc32765ad1cd0b
danec <danejcamacho@gmail.com>
2024-10-27 21:30:51 -0700

    added clamping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,18 +10,15 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5.0f;
     public UnityEvent shootBullet;
     public GameObject bulletPrefab;
+    private float clampedX;
 
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.GetKey(KeyCode.A)){
-            transform.position += Vector3.left * speed * Time.deltaTime;
-        }
-
-        if(Input.GetKey(KeyCode.D)){
-            transform.position += Vector3.right * speed * Time.deltaTime;
-        }
+    {   
+        clampedX = Mathf.Clamp(transform.position.x, -8, 8);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+        transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
         if(Input.GetKeyDown(KeyCode.Space)){
             shootBullet.Invoke();
